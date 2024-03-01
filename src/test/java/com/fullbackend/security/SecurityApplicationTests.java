0c04e12d11c9c37103b7d87ece572b74128ca651
Diego-Galeano <diegogaleano.ar@gmail.com>
2024-03-01 13:45:26 -0300

    first commit

---
+++ src/test/java/com/fullbackend/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullbackend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
